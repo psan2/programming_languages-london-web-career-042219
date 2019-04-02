3b963bb07f74441a2294e4a4d285b6087ea7fbe4
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-04-02 22:05:00 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |style, language|
+    language.each do |lang, type|
+      string = type.values.join
+      if new_hash.has_key?(lang)
+        new_hash[lang][:style].push(style)
+      else
+        new_hash[lang] = {:type => string, :style => []}
+        new_hash[lang][:style].push(style)
+      end
+    end
+  end
+  new_hash
 end
