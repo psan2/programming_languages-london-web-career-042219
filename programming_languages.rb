4ab0d3ad84faec542b6360d1c231a0eac40de332
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-04-02 22:04:23 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,10 +7,10 @@ def reformat_languages(languages)
     language.each do |lang, type|
       string = type.values.join
       if new_hash.has_key?(lang)
-        new_hash[lang][:style] << style
+        new_hash[lang][:style].push(style)
       else
         new_hash[lang] = {:type => string, :style => []}
-        new_hash[lang][:style] << style
+        new_hash[lang][:style].push(style)
       end
     end
   end
