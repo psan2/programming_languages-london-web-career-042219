3aa798cdf48fe5f3f6a5c9e23f3e1da6c02eff70
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-04-02 21:25:52 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |style, language|
+    language.each do |lang, type|
+      new_hash[lang][:type] = type.values
+      binding.pry
+    end
+  end
 end
