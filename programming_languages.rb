94bc5862b42e098e65eef9149e28a5d270de0c52
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-04-02 22:02:59 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -10,7 +10,7 @@ def reformat_languages(languages)
       if new_hash.has_key?(lang)
         new_hash[lang][:style] << style
       else
-        new_hash[lang][:style] = []
+        new_hash[lang][:style] => []
         new_hash[lang][:style] << style
       end
     end
